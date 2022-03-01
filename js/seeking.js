09369eb0dbfd8aa61001fd6f3bf50a5f612da93a
Maisha589 <maisharahman589@gmail.com>
2022-03-02 04:14:01 +0600

    details

--- js/seeking.js
+++ js/seeking.js
@@ -13,9 +13,11 @@ const searchPhone = () => {
             .then(res => res.json())
             .then(data => displaySearchResult(data.data));
         document.getElementById("empty").style.display = "none";
-    }
-    console.log(searchText);
+
+    };
 }
+// console.log(searchText);
+
 const displaySearchResult = phones => {
     const searchResult = document.getElementById("phone-container");
     // clear previous result
@@ -53,21 +55,41 @@ const showDetails = phone => {
     console.log(phone);
     const details = document.getElementById("details");
     // clear previous details
+    details.textContent = "";
+
     const div = document.createElement("div");
     div.innerHTML = `
     <div class="card text-center border border-secondary rounded-3 m-3 w-100">
     <div class="row g-0">
         <div class="col-md-4">
-            <img src="${phone.image}" class="img-fluid rounded-start " alt="...">
+            <img src="${phone.image}" class="w-100 rounded-start p-5 " alt="...">
         </div>
         <div class="col-md-8">
             <div class="card-body">
                 <h2 class="card-title"><span class="fw-bold">${phone.name}</span></h2>
-                <h4>${phone.slug}</h4>
+                <h4><span class="text-secondary">Brand:</span> ${phone.brand}</h4>
                 <h4><span class="text-secondary">Release Date:</span> ${phone.releaseDate}</h4>
-                <h4><span class="text-secondary">Feature:</span>${phone.mainFeatures.chipSet}</h4>
-                <h4><span class="text-secondary">Sensor:</span>${phone.mainFeatures.sensors[0]}</h4>
-                <h4>${phone.others.USB}</h4>
+                <h4><span class="text-secondary">Chip:</span>${phone.mainFeatures.chipSet}</h4>
+                <h4><span class="text-secondary">Display:</span>${phone.mainFeatures.displaySize}</h4>
+                <h4><span class="text-secondary">Memory:</span>${phone.mainFeatures.memory}</h4>
+                <h4><span class="text-secondary">Face ID:</span>${phone.mainFeatures.sensors[0]}</h4>
+                <h4><span class="text-secondary">accelerometer:</span>${phone.mainFeatures.sensors[1]}</h4>
+                <h4><span class="text-secondary">gyro:</span>${phone.mainFeatures.sensors[2]}</h4>
+                <h4><span class="text-secondary">proximity:</span>${phone.mainFeatures.sensors[3]}</h4>
+                <h4><span class="text-secondary">proximity:</span>${phone.mainFeatures.sensors[4]}</h4>
+                <h4><span class="text-secondary">barometer:</span>${phone.mainFeatures.sensors[5]}</h4>
+                <h4><span class="text-secondary">Bluetooth:</span>${phone.others.Bluetooth}</h4>
+                <h4></h4>
+                <h4><span class="text-secondary">GPS:</span>${phone.others.GPS}</h4>
+                <h4></h4>
+                <h4><span class="text-secondary">NFC:</span>${phone.others.NFC}</h4>
+                <h4></h4>
+                <h4><span class="text-secondary">Radio:</span>${phone.others.Radio}</h4>
+                <h4></h4>
+                <h4><span class="text-secondary">USB:</span>${phone.others.USB}</h4>
+                <h4></h4>
+                <h4><span class="text-secondary">WLAN:</span>${phone.others.WLAN}</h4>
+                <h4></h4>
             </div>
         </div>
     </div>
