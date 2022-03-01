010a2a48a748e689f871ef844ed23878e273d0aa
Maisha589 <maisharahman589@gmail.com>
2022-03-02 05:21:01 +0600

    bonus

--- js/seeking.js
+++ js/seeking.js
@@ -3,19 +3,21 @@ const searchPhone = () => {
     const searchText = searcField.value;
     // clear data
     searcField.value = "";
-    if (searchText == "") {
-        document.getElementById("empty").style.display = "block";
-    }
-    else {
-        // load data
-        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => displaySearchResult(data.data));
-        document.getElementById("empty").style.display = "none";
 
-    };
-}
+    // load data
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            if (searchText == "") {
+                document.getElementById("empty").style.display = "block";
+            }
+            else {
+                displaySearchResult(data.data.slice(0, 20));
+                document.getElementById("empty").style.display = "none";
+            }
+        });
+};
 // console.log(searchText);
 
 const displaySearchResult = phones => {
@@ -28,7 +30,7 @@ const displaySearchResult = phones => {
         div.classList.add("col");
 
         div.innerHTML = `
-        <div class="card h-100 p-3 text-center border border-secondary rounded-3 m-3">
+        <div class="card shadow-lg h-100 p-3 text-center border border-secondary rounded-3 mb-3">
           <img class="w-50 p-3" src="${phone.image}" class="card-img-top" alt="...">
           <div class="card-body">
             <h4 class="card-title"> ${phone.phone_name}</h4>
@@ -59,7 +61,7 @@ const showDetails = phone => {
 
     const div = document.createElement("div");
     div.innerHTML = `
-    <div class="card text-center border border-secondary rounded-3 m-3 w-100">
+    <div class="card shadow-lg text-center border border-secondary rounded-3 m-3 w-100">
     <div class="row g-0">
         <div class="col-md-4">
             <img src="${phone.image}" class="w-100 rounded-start p-5 " alt="...">
@@ -68,16 +70,16 @@ const showDetails = phone => {
             <div class="card-body">
                 <h2 class="card-title"><span class="fw-bold">${phone.name}</span></h2>
                 <h4><span class="text-secondary">Brand:</span> ${phone.brand}</h4>
-                <h4><span class="text-secondary">Release Date:</span> ${phone.releaseDate}</h4>
+                <h4><span class="text-secondary">Release Date:</span> ${phone.releaseDate ? phone.releaseDate : 'Comming soon'}</h4>
                 <h4><span class="text-secondary">Chip:</span>${phone.mainFeatures.chipSet}</h4>
                 <h4><span class="text-secondary">Display:</span>${phone.mainFeatures.displaySize}</h4>
                 <h4><span class="text-secondary">Memory:</span>${phone.mainFeatures.memory}</h4>
-                <h4><span class="text-secondary">Face ID:</span>${phone.mainFeatures.sensors[0]}</h4>
-                <h4><span class="text-secondary">accelerometer:</span>${phone.mainFeatures.sensors[1]}</h4>
-                <h4><span class="text-secondary">gyro:</span>${phone.mainFeatures.sensors[2]}</h4>
-                <h4><span class="text-secondary">proximity:</span>${phone.mainFeatures.sensors[3]}</h4>
-                <h4><span class="text-secondary">proximity:</span>${phone.mainFeatures.sensors[4]}</h4>
-                <h4><span class="text-secondary">barometer:</span>${phone.mainFeatures.sensors[5]}</h4>
+                <h4><span class="text-secondary"></span>${phone.mainFeatures.sensors[0]}</h4>
+                <h4><span class="text-secondary"></span>${phone.mainFeatures.sensors[1] ? phone.mainFeatures.sensors[1] : 'Not found'}</h4>
+                <h4><span class="text-secondary"></span>${phone.mainFeatures.sensors[2] ? phone.mainFeatures.sensors[2] : 'Not found'}</h4>
+                <h4><span class="text-secondary"></span>${phone.mainFeatures.sensors[3] ? phone.mainFeatures.sensors[3] : 'Not found'}</h4>
+                <h4><span class="text-secondary"></span>${phone.mainFeatures.sensors[4] ? phone.mainFeatures.sensors[4] : 'Not found'}</h4>
+                <h4><span class="text-secondary"></span>${phone.mainFeatures.sensors[5] ? phone.mainFeatures.sensors[5] : 'Not found'}</h4>
                 <h4><span class="text-secondary">Bluetooth:</span>${phone.others.Bluetooth}</h4>
                 <h4></h4>
                 <h4><span class="text-secondary">GPS:</span>${phone.others.GPS}</h4>
